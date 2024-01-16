650384a9f75e29daa0f49cf96bcf9b2cbce614fa
Egor <vev_113@mail.ru>
2024-01-16 18:36:18 +0300

    Показ наибольшей цифры числа

---
+++ ДЗ3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод целого числа из отрезка [10, 99]
+        Console.Write("Введите целое число от 10 до 99: ");
+        int number = int.Parse(Console.ReadLine());
+
+        // Проверка, что число находится в заданном диапазоне
+        if (number >= 10 && number <= 99)
+        {
+            // Определение наибольшей цифры
+            int maxDigit = FindMaxDigit(number);
+
+            // Вывод результата
+            Console.WriteLine($"Наибольшая цифра в числе {number} - {maxDigit}.");
+        }
+        else
+        {
+            Console.WriteLine("Введенное число не находится в диапазоне [10, 99].");
+        }
+    }
+
+    static int FindMaxDigit(int number)
+    {
+        // Извлечение десятков и единиц
+        int tens = number / 10;
+        int units = number % 10;
+
+        // Определение наибольшей цифры
+        return Math.Max(tens, units);
+    }
+}
