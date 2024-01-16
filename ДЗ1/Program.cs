116f3e011910549f8be603e1d3cfc7262a806427
Egor <vev_113@mail.ru>
2024-01-16 17:58:23 +0300

    Проверка кратности 7 и 23

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ввод числа с клавиатуры
+        Console.Write("Введите число: ");
+        int userNumber = int.Parse(Console.ReadLine());
+
+        // Проверка кратности 7 и 23
+        if (CheckDivisibility(userNumber))
+        {
+            Console.WriteLine($"{userNumber} кратно и 7, и 23.");
+        }
+        else
+        {
+            Console.WriteLine($"{userNumber} не кратно одновременно 7 и 23.");
+        }
+    }
+
+    static bool CheckDivisibility(int number)
+    {
+        // Проверка кратности 7 и 23
+        if (number % 7 == 0 && number % 23 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+
